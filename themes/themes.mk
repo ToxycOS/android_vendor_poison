a39c0bb8b99d596dff8b55411059c26c312c8254
Toxyc <54903513+iamtoxyc@users.noreply.github.com>
2020-10-31 15:57:41 +0530

    poison: Include Themes

---
+++ themes/themes.mk
@@ -0,0 +1,16 @@
+# Themes
+PRODUCT_PACKAGES += \
+    ToxycClearSystemOverlay \
+    ToxycClearSystemUIOverlay \
+    PitchBlackSystemOverlay \
+    PitchBlackSystemUIOverlay \
+    SolarizedDarkSystemOverlay \
+    SolarizedDarkSystemUIOverlay \
+    BakedGreenSystemOverlay \
+    BakedGreenSystemUIOverlay \
+    ChocoXSystemOverlay \
+    ChocoXSystemUIOverlay \
+    MaterialOceanSystemOverlay \
+    MaterialOceanSystemUIOverlay \
+    DarkGreySystemOverlay \
+    DarkGreySystemUIOverlay
